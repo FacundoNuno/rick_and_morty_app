3d874e76d173b79ff7fab52ac6cab9cfe02ed9f0
facundoureta <facuureta1993@gmail.com>
2023-05-25 20:36:54 -0300

    Boton Home y About

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import './App.css';
 import Cards from './components/cards/Cards.jsx';
-import { useState } from 'react';
-import Nav from "./components/nav/NavBar"
-import axios from "axios"
+import { Component, useState } from 'react';
+import NavBar from "./components/nav/NavBar";
+import axios from "axios";
 import { Routes, Route } from "react-router-dom";
-
-
+import About from "./components/about/About";
+import Detail from "./components/detail/Detail"
 
 function App() {
    const [characters, setCharacters] = useState([])
-
    const onClose = id => {
       setCharacters(characters.filter(caracter =>
          caracter.id !== Number(id)))
    }
-   
    function generarRandomId () {
       const randomId = Math.floor(Math.random() * 826) + 1;
       onSearch(randomId)
    } 
-
    function onSearch(id) {
       axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
          if (data.name) {
@@ -34,9 +31,16 @@ function App() {
       });
    }
       return (
-      <div className='App'>
-         <Nav onSearch={onSearch} randomCharacter={generarRandomId} />
-         <Cards onClose={onClose} characters={characters} />
+         
+         
+         <div className='App'>
+         <NavBar onSearch={onSearch} randomCharacter={generarRandomId} />
+         <Routes>
+            <Route path="/home" element={<Cards onClose={onClose} characters={characters} />}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/detail/:id" element={<Detail/>}/>
+         </Routes>
+         
       </div>
    );
    }
--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,15 +1,23 @@
 import SearchBar  from "../searchBar/SearchBar";
+import { Link, NavLink } from "react-router-dom"
 
 function NavBar ({onSearch, randomCharacter}) {
     function generarRandomId() {
         randomCharacter();
     }
-        
+    
     
     return ( 
         <div>
             <SearchBar onSearch={onSearch}/>
             <button onClick={generarRandomId}>Random</button>
+                <Link to='/home'>
+                    <button>Home</button>
+                </Link>
+                <NavLink to='/about'>
+                    <button>About</button>
+                </NavLink>
+            
         </div>
     );
 }
